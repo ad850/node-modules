380431357b44cd8f6283bd9e64860ef4e81b5a0b
aditya <aditya.sharma9827.as@gmail.com>
2021-12-29 13:35:01 +0530

    updated

---
+++ index.js
@@ -0,0 +1,8 @@
+//......................................path module
+
+const path = require("path");
+// console.log(path.dirname("D:/backend/index.js"));
+// console.log(path.extname("D:/backend/index.js"));
+// console.log(path.basename("D:/backend/index.js"));
+//console.log(path.parse("D:/backend/index.js"));
+
